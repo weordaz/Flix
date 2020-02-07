132d52d10d4665f36e8bcaad7a96cd85e5f2c47b
William Ordaz <william_ordaz@yahoo.com>
2020-02-06 19:11:49 -0800

    Flix Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by William Ordaz on 2/5/20.
+//  Copyright © 2020 William Ordaz. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
